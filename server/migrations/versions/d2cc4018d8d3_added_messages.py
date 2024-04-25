37db9aa24e695850987719448442a3969d790750
Spencer <spinsab@gmail.com>
2024-04-25 10:05:07 -0400

    chatterbox lab

---
+++ server/migrations/versions/d2cc4018d8d3_added_messages.py
@@ -0,0 +1,24 @@
+"""added messages
+
+Revision ID: d2cc4018d8d3
+Revises: 3aa8ba6c9aa7
+Create Date: 2024-04-24 17:06:02.131233
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd2cc4018d8d3'
+down_revision = '3aa8ba6c9aa7'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
